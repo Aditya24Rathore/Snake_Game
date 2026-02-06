81f4e3dd0257738e9825e31c4291da134ebcd174
Aditya Rathore <rathoreaditya262@gmail.com>
2026-02-06 22:04:14 +0530

    Implement Snake constructor and setDirection method with direction validation

--- snake.cpp
+++ snake.cpp
@@ -4,3 +4,15 @@ Snake::Snake(int startX, int startY) {
   body.push_back({startX, startY});
   dir = STOP;
 }
+
+void Snake::setDirection(Direction d) {
+  
+  ////Prevent opposite direction movements
+  if((dir == LEFT && d == RIGHT)  ||
+      (dir == RIGHT && d == LEFT) ||
+      (dir == UP && d == DOWN)  ||
+      (dir == DOWN && d == UP))
+      return;
+
+  dir = d;
+}
